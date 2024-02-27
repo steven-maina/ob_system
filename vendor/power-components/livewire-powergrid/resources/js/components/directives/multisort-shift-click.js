75294c95f22008cd4374515258832df28f9402bc
unknown <stevenmaina17@gmail.com>
2024-02-27 23:26:58 +0300

    uploads

--- vendor/power-components/livewire-powergrid/resources/js/components/directives/multisort-shift-click.js
+++
@@ -1,5 +0,0 @@
-window.Alpine.directive('multisort-shift-click', (el, { expression }) => {
-    el.addEventListener('click', (event) => {
-        window.Livewire.find(expression).set('multiSort', event.shiftKey)
-    })
-})
--- vendor/power-components/livewire-powergrid/resources/js/index.js
+++
@@ -1,3 +0,0 @@
-import './stores'
-import './components'
-import './components/directives'
